3b35329d3aa60bde18ad75f02697f2c79b32aa8a
RohitKV1n0d <rohitvinod92@gmail.com>
2021-06-25 12:09:21 +0530

    commit rest of the pages

--- app.py
+++ app.py
@@ -3,7 +3,43 @@ from flask import Flask, render_template
 app = Flask(__name__,template_folder="templates")
 
 @app.route('/')
-def index():
+@app.route('/welcome')
+def welcome():
     return render_template("landing-page.html")
+
+@app.route('/login.html')
+def login():
+    return render_template("login.html")
+
+@app.route('/register.html')
+def register():
+    return render_template("register.html")
+
+@app.route('/rules.html')
+def rules():
+    return render_template("rules.html")
+
+@app.route('/start.html')
+def start():
+    return render_template("start-page.html")
+    
+
+@app.route('/key1.html')
+def round1():
+    return render_template("round1.html")
+
+
+
+@app.route('/key2.html')
+def round2():
+    return render_template("round2.html")
+
+@app.route('/key3.html')
+def round3():
+    return render_template("round3.html")
+
+
+
+
 
     
